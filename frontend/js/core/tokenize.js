8c1a4b013038da3e3d31d6985fdf530976721019
md-haseb <rahman.mdhaseb@gmail.com>
2026-01-03 15:33:21 +0600

    fix(tokenizer): correctly tokenize numbers and super/subscripts before combinatorics

--- frontend/js/core/tokenize.js
+++ frontend/js/core/tokenize.js
@@ -19,11 +19,14 @@ export function tokenize(expr){
   `|(?:\\d+(?:\\.\\d+)?e)` + //numbers with e
   `|(?:\\d+(?:\\.\\d+)?π)` + //numbers with pi
   `|[${superscriptChars}]+√` +  // nth-root operator like 3√, 7√
+  `|(?:\\d+\\.\\d+|\\d+|\\.\\d*)(?=[${superscriptChars}${subscriptChars}]+[CP])` + //NUMBER before C / P
   `|(?:\\d+\\.\\d+|\\d+|\\.\\d*)[${superscriptChars}${subscriptChars}]*` + // numbers with optional super/subscripts
+  // `|(?:\\d+\\.\\d+|\\d+|\\.\\d*)` + //normal number
   `|[${superscriptChars}]+` + // consecutive standalone super/subscripts
   `|[${subscriptChars}]+` +
   `|[+\\-*/√%!()]`,                           // operators
   'g');
+
   // console.log(expr.match(tokenRegEx));
   // return expr.match(tokenRegEx);
   // const rawTokens = expr.match(tokenRegEx) || [];
