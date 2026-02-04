910f2ae23d3ce55f15d6386386dc46e001d43b2f
md-haseb <rahman.mdhaseb@gmail.com>
2026-02-04 17:05:25 +0600

    fix(history): prevent updating history on incomplete expression

--- frontend/js/ui/history.js
+++ frontend/js/ui/history.js
@@ -97,12 +97,15 @@ export function appendHistory(input, lastExpHTML, historyContainer, historyRemov
   const result = input.innerHTML;
   const currentHistory = getHistory();
 
-  if (expression && result) {
-    currentHistory.push(historyObj(expression, result));
-    saveHistory(currentHistory);
-  }
-  if(currentHistory.length > 0) {
-    updateRemoveBtn(currentHistory.length, historyRemoveBtn);
+  if (expression != result) {
+    if (expression && result) {
+      currentHistory.push(historyObj(expression, result));
+      saveHistory(currentHistory);
+    }
+
+    if(currentHistory.length > 0) {
+      updateRemoveBtn(currentHistory.length, historyRemoveBtn);
+    }
   }
   renderHistory(currentHistory, historyContainer);
 }
